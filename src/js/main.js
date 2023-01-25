8e7c12e7a451abee5fe8b9ce6ba12b014119beb8
Joaquín Uriel García <82168815+JoaquinDev542@users.noreply.github.com>
2023-01-25 23:43:46 +0100

    html,css,js

--- src/js/main.js
+++ src/js/main.js
@@ -38,3 +38,46 @@ const functionChangeBgHeader = ()=> {
     }
 }
 window.addEventListener("scroll" , functionChangeBgHeader);
+
+// Work Hover Projects
+let projectImgTextFirst = document.querySelector(".projectImgText--first");
+let projectImgTextSecond = document.querySelector(".projectImgText--second");
+let projectImgTextThird = document.querySelector(".projectImgText--third");
+let projectImg__imgFirst = document.querySelector(".projectImg__img--first");
+let projectImg__imgSecond = document.querySelector(".projectImg__img--second");
+let projectImg__imgThird = document.querySelector(".projectImg__img--third");
+
+let projectContent__linkFirst = document.querySelector(".projectContent__link--first");
+let projectContent__linkSecond = document.querySelector(".projectContent__link--second");
+let projectContent__linkThird = document.querySelector(".projectContent__link--third");
+
+
+projectContent__linkFirst.addEventListener("mouseover" , ()=> {
+    projectImgTextFirst.classList.add("active");
+    projectImg__imgFirst.classList.add("active");
+});
+
+projectContent__linkFirst.addEventListener("mouseout" , ()=> {
+    projectImgTextFirst.classList.remove("active");
+    projectImg__imgFirst.classList.remove("active");
+});
+
+projectContent__linkSecond.addEventListener("mouseover" , ()=> {
+    projectImgTextSecond.classList.add("active");
+    projectImg__imgSecond.classList.add("active");
+});
+
+projectContent__linkSecond.addEventListener("mouseout" , ()=> {
+    projectImgTextSecond.classList.remove("active");
+    projectImg__imgSecond.classList.remove("active");
+});
+
+projectContent__linkThird.addEventListener("mouseover" , ()=> {
+    projectImgTextThird.classList.add("active");
+    projectImg__imgThird.classList.add("active");
+});
+
+projectContent__linkThird.addEventListener("mouseout" , ()=> {
+    projectImgTextThird.classList.remove("active");
+    projectImg__imgThird.classList.remove("active");
+});
