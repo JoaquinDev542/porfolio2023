5019d5d2cf92eee63188a10320490ff23dd0dedb
Joaquín Uriel García <82168815+JoaquinDev542@users.noreply.github.com>
2023-01-29 20:38:47 +0100

    active links javascript

--- src/js/main.js
+++ src/js/main.js
@@ -27,6 +27,76 @@ menuOpenList__linkContact.addEventListener("click" , () => {
     menuOpen.classList.remove("active");
 });
 
+// Menu Links when active state
+
+let headerLink = document.querySelector(".headerLink");
+let headerLinkFooter = document.querySelector(".headerLink--footer");
+let navList__linkAbout = document.querySelector(".navList__link--about" , "::after");
+let navList__linkSkills = document.querySelector(".navList__link--skills"  , "::after");
+let navList__linkProjects = document.querySelector(".navList__link--projects"  , "::after");
+let navList__linkContact = document.querySelector(".navList__link--contact"  , "::after");
+// let footerContainerDarkMode__link = document.querySelector(".footerContainerDarkMode__link");
+
+headerLink.addEventListener('click' , ()=> {
+    navList__linkAbout.classList.remove("active");
+    navList__linkSkills.classList.remove("active");
+    navList__linkProjects.classList.remove("active");
+    navList__linkContact.classList.remove("active");
+});
+
+navList__linkAbout.addEventListener('click' , ()=> {
+    navList__linkAbout.classList.add("active");
+    navList__linkSkills.classList.remove("active");
+    navList__linkProjects.classList.remove("active");
+    navList__linkContact.classList.remove("active");
+});
+
+navList__linkSkills.addEventListener('click' , ()=> {
+    navList__linkSkills.classList.add("active");
+    navList__linkAbout.classList.remove("active");
+    navList__linkProjects.classList.remove("active");
+    navList__linkContact.classList.remove("active");
+});
+
+navList__linkProjects.addEventListener('click' , ()=> {
+    navList__linkProjects.classList.add("active");
+    navList__linkAbout.classList.remove("active");
+    navList__linkSkills.classList.remove("active");
+    navList__linkContact.classList.remove("active");
+});
+
+navList__linkContact.addEventListener('click' , ()=> {
+    navList__linkContact.classList.add("active");
+    navList__linkAbout.classList.remove("active");
+    navList__linkSkills.classList.remove("active");
+    navList__linkProjects.classList.remove("active");
+});
+
+headerLinkFooter.addEventListener('click' , ()=> {
+    navList__linkAbout.classList.remove("active");
+    navList__linkSkills.classList.remove("active");
+    navList__linkProjects.classList.remove("active");
+    navList__linkContact.classList.remove("active");
+});
+
+// footerContainerDarkMode__link.addEventListener('click' , ()=> {
+//     navList__linkAbout.classList.remove("active");
+//     navList__linkSkills.classList.remove("active");
+//     navList__linkProjects.classList.remove("active");
+//     navList__linkContact.classList.remove("active");
+// });
+
+
+// const functionDeleteLinksActive = ()=> {
+//     if(document.documentElement.scrollTop >= .1) {
+//         navList__linkSkills.classList.remove("active");
+//         navList__linkAbout.classList.remove("active");
+//         navList__linkProjects.classList.remove("active");
+//         navList__linkContact.classList.remove("active");
+// }
+// window.addEventListener("scroll" , functionDeleteLinksActive);
+
+
 // Header Bg Section
 let header = document.querySelector(".header");
 
